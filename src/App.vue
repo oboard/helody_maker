4f388f411a489b2e3cdcdcf1f291f1c8612aa1b2
oboard <oboard@outlook.com>
2023-03-09 22:10:48 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,380 @@
+<template>
+  <div class="flex flex-col">
+    <div class="flex flex-row">
+      <div class="flex flex-col flex-1">
+        <Card class="flex-1" style="width:100%">
+          <template #title>
+            <Icon type="ios-musical-notes"></Icon>
+            音乐控制器
+          </template>
+          <audio class="hidden" ref="audio" controls="controls" :preload="preload">
+            <source />
+          </audio>
+          <h4 style="width: 100%;height: 50px;line-height: 50px; text-align: center;">{{ namesong }}</h4>
+          <div class="flex justify-center">
+            <ButtonGroup shape="circle">
+              <Button type="info" title="上一首" size="large" @click="up(Indexsong)">
+                <Icon type="ios-arrow-back" />
+              </Button>
+              <Button type="info" title="播放/暂停" size="large" @click="play(namesong, Indexsong)">
+                <Icon :type="playButton" />
+              </Button>
+              <Button type="info" title="下一首" size="large" @click="down(Indexsong)">
+                <Icon type="ios-arrow-forward" />
+              </Button>
+              <Button type="info" title="列表" size="large" @click="IssongListshowhide">
+                <Icon type="ios-list" />
+              </Button>
+            </ButtonGroup>
+          </div>
+          <Table :columns="columns" :data="songList" v-show="songListhidden" @click="IssongListshowhide"></Table>
+        </Card>
+        <Card class="flex-1" style="width:100%">
+          <template #title>
+            <Icon type="ios-musical-notes"></Icon>
+              片段控制器
+            </template>
+            <div class="flex justify-center">
+            <ButtonGroup shape="circle">
+                <Button type="info" title="删除此片段" size="large" @click="deleteClip">
+                  <Icon type="ios-trash" />删除
+                </Button>
+                <Button type="info" title="清空此片段" size="large" @click="clearClip">
+                  <Icon type="ios-trash-outline" />清空
+                </Button>
+                <Button type="info" title="添加片段" size="large" @click="addClip">
+                  <Icon type="ios-add" />添加
+                </Button>
+              </ButtonGroup>
+              </div>
+            <Form :model="clipControllerData" :label-width="80">
+              <FormItem label="片段">
+              <Slider v-model="editingClipIndex" :min="1" :max="formItem.clips.length" show-input step></Slider>
+            </FormItem>
+            <FormItem label="BPM">
+              <Slider v-model="formItem.clips.bpm" :min="1" :max="300" show-input></Slider>
+            </FormItem>
+          </Form>
+          
+        </Card>
+      </div>
+      <Card class="flex-1" style="width:100%">
+        <template #title>
+          <Icon type="ios-musical-notes"></Icon>
+          参数设置
+        </template>
+        <Form :model="formItem" :label-width="80">
+          <FormItem label="歌名">
+            <Input v-model="formItem.songName" placeholder="Enter something..."></Input>
+          </FormItem>
+          <!-- <FormItem label="Select">
+            <Select v-model="formItem.select">
+              <Option value="beijing">New York</Option>
+              <Option value="shanghai">London</Option>
+              <Option value="shenzhen">Sydney</Option>
+            </Select>
+          </FormItem> -->
+          <FormItem label="创作日期">
+            <!-- <Row> -->
+            <!-- <Col span="11"> -->
+            <DatePicker type="date" placeholder="选择日期" v-model="formItem.createDate"></DatePicker>
+            <!-- </Col> -->
+            <!-- <Col span="2" style="text-align: center">-</Col>
+              <Col span="11">
+              <TimePicker type="time" placeholder="选择时间" v-model="formItem.time"></TimePicker> -->
+            <!-- </Col> -->
+            <!-- </Row> -->
+          </FormItem>
+          <!-- <FormItem label="Radio">
+            <RadioGroup v-model="formItem.radio">
+              <Radio label="male">Male</Radio>
+              <Radio label="female">Female</Radio>
+            </RadioGroup>
+          </FormItem>
+          <FormItem label="Checkbox">
+            <CheckboxGroup v-model="formItem.checkbox">
+              <Checkbox label="Eat"></Checkbox>
+              <Checkbox label="Sleep"></Checkbox>
+              <Checkbox label="Run"></Checkbox>
+              <Checkbox label="Movie"></Checkbox>
+            </CheckboxGroup>
+          </FormItem> -->
+          <!-- <FormItem label="Switch">
+            <i-switch v-model="formItem.switch" size="large">
+              <template #open>
+                <span>On</span>
+              </template>
+              <template #close>
+                <span>Off</span>
+              </template>
+            </i-switch>
+          </FormItem> -->
+          <FormItem label="难度等级">
+            <Slider v-model="formItem.difficulty
+            " :min="1" :max="20" show-input></Slider>
+          </FormItem>
+          <FormItem label="歌手">
+            <Input v-model="formItem.artist" placeholder="Enter something..."></Input>
+          </FormItem>
+          <FormItem label="谱师">
+            <Input v-model="formItem.description" placeholder="Enter something..."></Input>
+          </FormItem>
+          <FormItem label="简介">
+            <Input v-model="formItem.description" type="textarea" :autosize="{ minRows: 2, maxRows: 5 }"
+              placeholder="Enter something..."></Input>
+          </FormItem>
+          <FormItem>
+            <Button type="primary" @click="handleSubmit">导出</Button>
+            <!-- <Button style="margin-left: 8px">Cancel</Button> -->
+          </FormItem>
+        </Form>
+      </Card>
+    </div>
+    <div class="overflow-x-scroll p-8">
+      <span>空格：打点</span>
+    </div>
+    <Card class="" style="width:100%">
+      <template #title>
+        <Icon type="ios-infinite" />
+        波形显示器
+      </template>
+      <div id="zoomview-container" class="h-16" ref="zoomview"></div>
+      <div id="overview-container" class="h-16" ref="overview"></div>
+    </Card>
+  </div>
+</template>
+  
+<script>
+import Peaks from 'peaks.js';
+
+export default {
+  data() {
+    return {
+      formItem: {
+        songName: '',
+        select: '',
+        radio: 'male',
+        checkbox: [],
+        switch: true,
+        createDate: '',
+        slider: 100,
+        artist: '',
+        artistUnicode: '',
+        creator: '',
+        version: '',
+        difficulty: 1,
+        description: '',
+        clips: [
+          {
+            bpm: 100,
+            index: 0,
+            from:0,
+            to:0,
+          },
+        ]
+      },
+      editingClipIndex: 0,
+      optiontype: ["up", "play", "down"],
+      preload: 'auto',
+      Indexsong: 0,
+      namesong: '',
+      playButton: 'ios-play',
+      bpm: 90,
+      musicUrl: '',
+      path: '/Music/',
+      columns: [
+        {
+          type: 'index',
+          title: '序号',
+          align: 'center',
+          width: 100,
+          render: (h, params) => {
+            return h(
+              'span',
+              params.index
+            )
+          }
+        },
+        {
+          title: '歌曲',
+          key: 'song'
+        },
+        {
+          title: '操作',
+          render: (h, params) => {
+            return h('div', [
+
+              h(
+                'Button',
+                {
+                  props: {
+                    type: 'info',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.play(params.row.song, params.index);
+                    }
+                  }
+                },
+                '播放'
+
+              )
+
+            ])
+          }
+        }
+      ],
+      songList: [
+        {
+          song: 'Beat Thee.mp3',
+
+        },
+        {
+          song: 'Bit Bit Loop.mp3',
+        },
+      ],
+
+      songListhidden: false
+    }
+  },
+  // computed: {  ///存在问题，未能 到底预期效果以换他方式created里实现
+  //     namesong:{
+  //       // setter
+  //       get() {    
+  //         console.log("get:"+)
+  //         return this.songList[1].song;
+  //       }
+  //       ,
+  //        set(newval) {  
+  //         console.log("set:"+newval)  
+  //         return newval;
+  //       }
+  //       }
+
+  //   },
+
+
+  methods: {
+    //切换上一曲
+    up(index) {
+      let vm = this;
+      if (index === 0) {
+        this.$Message.success({ title: '提示', content: '已经到顶了喔' })
+        return
+      }
+
+      vm.Indexsong = --index;
+      vm.playButton = 'ios-pause';
+      vm.namesong = vm.songList[vm.Indexsong].song;
+      console.log("Indexsong:" + vm.Indexsong + ",namesong:" + vm.namesong)
+      let audioplay = this.$refs.audio//播放
+
+      vm.musicUrl = this.path + vm.namesong
+      audioplay.src = vm.musicUrl;
+      audioplay.play();
+    },
+    //切换下一曲
+    down(index) {
+
+      let vm = this;
+      if (index === vm.songList.length - 1) {
+        this.$Message.success({ title: '提示', content: '已经到底了喔' })
+        return
+      }
+
+      vm.Indexsong = ++index;
+      vm.playButton = 'ios-pause';
+      vm.namesong = vm.songList[vm.Indexsong].song;
+      console.log("Indexsong:" + vm.Indexsong + ",namesong:" + vm.namesong)
+      let audioplay = this.$refs.audio
+
+      vm.musicUrl = this.path + vm.namesong
+      audioplay.src = vm.musicUrl;
+      audioplay.play();//播放
+
+    },
+    play(song, index) {
+
+      let vm = this;
+      vm.Indexsong = index;
+      console.log("Indexsong:" +
+        vm.Indexsong + ",song:" + song + ",+playButton:" + vm.playButton)
+      if (vm.namesong === song) {
+
+        if (vm.playButton === "ios-play") {
+          vm.playButton = 'ios-pause';
+          let audioplay = this.$refs.audio
+          // vm.musicUrl = this.path + vm.namesong
+          // audioplay.src = vm.musicUrl;
+          audioplay.play();//播放
+        } else {
+          vm.playButton = "ios-play";
+          this.$refs.audio.pause();//暂停    
+        }
+      } else if (vm.namesong != song) {
+
+        console.log("song:" + song)
+        console.log("playButton:" + vm.playButton)
+        vm.playButton = 'ios-pause';
+        vm.namesong = song;
+        let audioplay = this.$refs.audio
+        console.log("namesong:" + vm.namesong)
+        vm.musicUrl = this.path + vm.namesong
+        audioplay.src = vm.musicUrl;
+        audioplay.play();//播放
+      }
+
+    },
+    handleSubmit(name) {
+      console.log(Date.parse(this.formItem.createDate).toString());
+
+      // this.$refs[name].validate((valid) => {
+      //   if (valid) {
+      //     this.$Message.success('Success!');
+      //   } else {
+      //     this.$Message.error('Fail!');
+      //   }
+      // });
+    },
+    init() {
+      this.up(1);
+
+      const options = {
+        zoomview: {
+          container: this.$refs.zoomview, //缩略图
+        },
+        overview: {
+          container: this.$refs.overview,
+        },
+        mediaElement: this.$refs.audio,
+        webAudio: {
+          audioContext: new AudioContext(),
+        },
+        emitCueEvents: true,
+      };
+      Peaks.init(options, (err, peaks) => {
+        if (err) return;
+      });
+    },
+    IssongListshowhide() {
+      let vm = this;
+      vm.songListhidden = !vm.songListhidden;
+    }
+  }
+  ,
+  created() {
+    //赋值变量
+    this.namesong = this.songList[1].song;
+    this.Indexsong = 1;
+    this.$nextTick(() => {
+      this.init();
+    });
+  }
+
+
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './index.css'
+import App from './App.vue'
+import ViewUIPlus from 'view-ui-plus'
+import 'view-ui-plus/dist/styles/viewuiplus.css'
+
+createApp(App).use(ViewUIPlus).mount('#app')
